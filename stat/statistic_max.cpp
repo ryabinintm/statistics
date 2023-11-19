a14fc30951478186bf4df39d6e6987c8e84698bd
Alexander Ryabinin <ryabinintm@yandex.ru>
2023-11-19 16:38:28 +0300

    fix: statistic_max

--- stat/statistic_max.cpp
+++ stat/statistic_max.cpp
@@ -1,13 +1,14 @@
 #include "statistics.h"
 #include <limits>
+#include <cmath>
 
-Max::Max() : m_max{std::numeric_limits<double>::min()} {
+Max::Max() : m_max{std::numeric_limits<double>::lowest()} {
 }
 
 void Max::update(double next) {
-	if (next > m_max) {
-		m_max = next;
-	}
+  if (next > m_max) {
+    m_max = next;
+  }
 }
 
 double Max::eval() const {
